e5e5f29525237ea58d572ca96a5ce41be92ca246
Ubuntu <ubuntu@ip-172-31-41-244.ec2.internal>
2019-07-12 13:22:25 +0000

    main() function added & called

--- boggle.py
+++ boggle.py
@@ -92,3 +92,20 @@ def get_dictionary(dictionary_file):
     """
     with open(dictionary_file) as f:
         return [w.strip().upper() for w in f]
+
+
+def main():
+    """
+    This is the function that will run the whole project
+    """
+    grid = make_grid(3, 3)
+    dictionary = get_dictionary('words.txt')
+    words = search(grid, dictionary)
+    for word in words:
+        print(word)
+    print("Found %s words" % len(words))
+
+# the below if statement only runs main() if boggle.py is called in the command line (python3 boggle.py). If this is run from another file
+# (eg pyhton3 -m unittest) it will not be run
+if __name__ == "__main__":
+    main()
